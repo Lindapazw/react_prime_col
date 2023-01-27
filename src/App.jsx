10c7967b3150360500fdb379d33e154c41159fb9
Linda Paz <lindacsndelapaz@gmail.com>
2023-01-27 09:32:30 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div></div>
+  )
+}
+
+export default App
